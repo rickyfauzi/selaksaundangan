1ba2f9790b5103d632c7b393f4c7ff948084d926
rickyfauzi <rickyfauzi116@gmail.com>
2025-06-24 21:08:47 +0700

    hde

--- resources/views/undangan/bukutamu.blade.php
+++ resources/views/undangan/bukutamu.blade.php
@@ -75,15 +75,13 @@
                                                         </div>
                                                         <hr>
                                                         <div class="card-body">
-<div class="kirimtamu">
-    {{-- Blade akan mencetak URL dasar yang benar di sini --}}
-    <a href="https://blanchedalmond-elk-754408.hostingersite.com/wedding/{{ $order ? $order->domain : '' }}?to=" 
-       target="_BLANK"
-       id="tautan"
-       style="font-weight: 700">
-        https://blanchedalmond-elk-754408.hostingersite.com/wedding/{{ $order ? $order->domain : '' }}?to=
-    </a>
-</div>
+                                                            <div class="kirimtamu">
+                                                                {{-- Blade akan mencetak URL dasar yang benar di sini --}}
+                                                                <a href="selaksadigital.com/wedding/{{ $order ? $order->domain : '' }}?to="
+                                                                    target="_BLANK" id="tautan" style="font-weight: 700">
+                                                                    selaksadigital.com/wedding/{{ $order ? $order->domain : '' }}?to=
+                                                                </a>
+                                                            </div>
                                                             <label for="judulcerita1"
                                                                 style="color: #3D5782; margin-top:20px">Masukan Nama
                                                                 Tamu</label>
@@ -129,37 +127,37 @@
                                                                                     fill="white" stroke="none">
                                                                                     <path
                                                                                         d="M1110 4694 c-84 -17 -229 -90 -305 -151 -162 -131 -260 -325 -272
-                                                                                                -538 -8 -139 13 -239 79 -375 44 -91 62 -115 142 -195 76 -76 106 -99 186
-                                                                                                -138 121 -59 207 -80 335 -80 136 0 262 31 361 88 39 23 42 28 64 112 44 170
-                                                                                                156 357 277 460 l56 48 -6 90 c-14 205 -113 399 -272 528 -77 63 -220 133
-                                                                                                -310 152 -87 18 -249 18 -335 -1z" />
+                                                                                                    -538 -8 -139 13 -239 79 -375 44 -91 62 -115 142 -195 76 -76 106 -99 186
+                                                                                                    -138 121 -59 207 -80 335 -80 136 0 262 31 361 88 39 23 42 28 64 112 44 170
+                                                                                                    156 357 277 460 l56 48 -6 90 c-14 205 -113 399 -272 528 -77 63 -220 133
+                                                                                                    -310 152 -87 18 -249 18 -335 -1z" />
                                                                                     <path
                                                                                         d="M3670 4695 c-85 -19 -229 -91 -305 -152 -150 -122 -251 -310 -271
-                                                                                                -508 l-6 -61 43 -29 c191 -125 352 -384 384 -612 6 -48 9 -52 48 -67 93 -35
-                                                                                                173 -49 277 -49 133 0 217 20 340 80 80 39 110 62 186 138 80 80 98 104 142
-                                                                                                195 66 136 87 236 79 375 -18 318 -228 583 -538 680 -75 24 -292 30 -379 10z" />
+                                                                                                    -508 l-6 -61 43 -29 c191 -125 352 -384 384 -612 6 -48 9 -52 48 -67 93 -35
+                                                                                                    173 -49 277 -49 133 0 217 20 340 80 80 39 110 62 186 138 80 80 98 104 142
+                                                                                                    195 66 136 87 236 79 375 -18 318 -228 583 -538 680 -75 24 -292 30 -379 10z" />
                                                                                     <path
                                                                                         d="M2473 3930 c-184 -33 -357 -138 -468 -285 -63 -84 -95 -145 -127
-                                                                                                -245 -19 -64 -23 -96 -23 -215 0 -154 14 -213 77 -338 214 -424 782 -541 1144
-                                                                                                -235 277 234 350 610 181 933 -31 60 -65 103 -132 170 -101 101 -196 158 -325
-                                                                                                195 -86 24 -247 34 -327 20z" />
+                                                                                                    -245 -19 -64 -23 -96 -23 -215 0 -154 14 -213 77 -338 214 -424 782 -541 1144
+                                                                                                    -235 277 234 350 610 181 933 -31 60 -65 103 -132 170 -101 101 -196 158 -325
+                                                                                                    195 -86 24 -247 34 -327 20z" />
                                                                                     <path
                                                                                         d="M769 3140 c-379 -80 -671 -375 -749 -757 -19 -95 -20 -131 -18 -539
-                                                                                                l3 -437 150 -43 c168 -49 419 -105 570 -129 100 -16 351 -45 391 -45 21 0 22
-                                                                                                4 27 208 4 181 8 220 31 307 50 191 172 400 313 537 110 107 298 222 421 259
-                                                                                                23 7 42 15 42 19 0 3 -14 19 -31 36 -119 113 -225 346 -245 540 l-7 64 -406
-                                                                                                -1 c-352 0 -417 -3 -492 -19z" />
+                                                                                                    l3 -437 150 -43 c168 -49 419 -105 570 -129 100 -16 351 -45 391 -45 21 0 22
+                                                                                                    4 27 208 4 181 8 220 31 307 50 191 172 400 313 537 110 107 298 222 421 259
+                                                                                                    23 7 42 15 42 19 0 3 -14 19 -31 36 -119 113 -225 346 -245 540 l-7 64 -406
+                                                                                                    -1 c-352 0 -417 -3 -492 -19z" />
                                                                                     <path
                                                                                         d="M3526 3138 c-3 -13 -8 -48 -11 -78 -17 -151 -95 -330 -201 -461 -36
-                                                                                                -45 -64 -82 -62 -84 2 -1 29 -12 60 -23 268 -99 492 -301 623 -562 87 -174
-                                                                                                113 -293 122 -557 6 -189 6 -193 27 -188 12 2 66 7 121 10 294 19 631 96 838
-                                                                                                192 l77 36 0 430 c0 393 -2 437 -21 530 -77 385 -370 679 -753 758 -79 16
-                                                                                                -138 19 -453 19 l-363 0 -4 -22z" />
+                                                                                                    -45 -64 -82 -62 -84 2 -1 29 -12 60 -23 268 -99 492 -301 623 -562 87 -174
+                                                                                                    113 -293 122 -557 6 -189 6 -193 27 -188 12 2 66 7 121 10 294 19 631 96 838
+                                                                                                    192 l77 36 0 430 c0 393 -2 437 -21 530 -77 385 -370 679 -753 758 -79 16
+                                                                                                    -138 19 -453 19 l-363 0 -4 -22z" />
                                                                                     <path
                                                                                         d="M2177 2379 c-242 -25 -480 -153 -636 -341 -73 -87 -163 -261 -193
-                                                                                                -373 -21 -76 -22 -103 -26 -556 l-3 -476 123 -36 c262 -77 483 -124 728 -157
-                                                                                                199 -27 637 -38 814 -21 331 33 616 100 814 192 l84 39 -5 452 c-5 504 -8 529
-                                                                                                -74 688 -140 333 -440 553 -802 590 -119 12 -709 11 -824 -1z" />
+                                                                                                    -373 -21 -76 -22 -103 -26 -556 l-3 -476 123 -36 c262 -77 483 -124 728 -157
+                                                                                                    199 -27 637 -38 814 -21 331 33 616 100 814 192 l84 39 -5 452 c-5 504 -8 529
+                                                                                                    -74 688 -140 333 -440 553 -802 590 -119 12 -709 11 -824 -1z" />
                                                                                 </g>
                                                                             </svg>
                                                                         </div>
@@ -449,19 +447,19 @@
             getJumlahTamu()
             showTamu()
             $('#namatamu').on('input', function() {
-        // 1. Ambil URL dasar yang sudah benar dari Blade
-        const baseUrl = 'https://blanchedalmond-elk-754408.hostingersite.com/wedding/{{ $order ? $order->domain : '' }}?to=';
-        
-        // 2. Ambil nama tamu dari input dan pastikan aman untuk URL
-        const guestName = encodeURIComponent($(this).val());
-        
-        // 3. Gabungkan menjadi URL lengkap yang baru
-        const newURL = baseUrl + guestName;
+                // 1. Ambil URL dasar yang sudah benar dari Blade
+                const baseUrl = 'selaksadigital.com/wedding/{{ $order ? $order->domain : '' }}?to=';
 
-        // 4. Update elemen <a>
-        $('#tautan').attr('href', newURL);
-        $('#tautan').text(newURL);
-    })
+                // 2. Ambil nama tamu dari input dan pastikan aman untuk URL
+                const guestName = encodeURIComponent($(this).val());
+
+                // 3. Gabungkan menjadi URL lengkap yang baru
+                const newURL = baseUrl + guestName;
+
+                // 4. Update elemen <a>
+                $('#tautan').attr('href', newURL);
+                $('#tautan').text(newURL);
+            })
             $('#bukutamu-submit').on('submit', function(e) {
                 e.preventDefault()
                 $.ajax({
@@ -680,7 +678,7 @@
                 .replace(/{nama_group}/g, group)
                 .replace(/{kode_tamu}/g, kodetamu)
                 .replace(/{link_undangan}/g,
-                    'https://blanchedalmond-elk-754408.hostingersite.com/wedding/{{ $order ? $order->domain : '' }}' +
+                    'selaksadigital.com/wedding/{{ $order ? $order->domain : '' }}' +
                     nama);
 
             const convertedValue = pesanLinkUndangan.replace(/\\n/g, '\n\n');
@@ -690,40 +688,41 @@
             dummy.remove();
         }
 
-       const kirimPesan = (kodetamu, nama, group, whatsapp) => {
-    // 1. Validasi: Pastikan nama tamu tidak kosong
-    if (!nama) {
-        Swal.fire('Peringatan', 'Nama tamu tidak boleh kosong untuk membuat link.', 'warning');
-        return;
-    }
+        const kirimPesan = (kodetamu, nama, group, whatsapp) => {
+            // 1. Validasi: Pastikan nama tamu tidak kosong
+            if (!nama) {
+                Swal.fire('Peringatan', 'Nama tamu tidak boleh kosong untuk membuat link.', 'warning');
+                return;
+            }
 
-    // 2. Ambil template pesan dari atribut data
-    const text = $('#btn-copy').data('text');
+            // 2. Ambil template pesan dari atribut data
+            const text = $('#btn-copy').data('text');
 
-    // 3. Buat link undangan yang benar dan dinamis
-    //    Ini adalah bagian utama yang diperbaiki
-    const linkUndangan = 'https://blanchedalmond-elk-754408.hostingersite.com/wedding/{{ $order ? $order->domain : '' }}?to=' + encodeURIComponent(nama);
+            // 3. Buat link undangan yang benar dan dinamis
+            //    Ini adalah bagian utama yang diperbaiki
+            const linkUndangan = 'selaksadigital.com/wedding/{{ $order ? $order->domain : '' }}?to=' +
+                encodeURIComponent(nama);
 
-    // 4. Ganti semua variabel di dalam template
-    const pesanLinkUndangan = text
-        .replace(/{nama_tamu}/g, nama)
-        .replace(/{nama_group}/g, group || '') // Fallback jika group null
-        .replace(/{kode_tamu}/g, kodetamu || '') // Fallback jika kode tamu null
-        .replace(/{link_undangan}/g, linkUndangan);
+            // 4. Ganti semua variabel di dalam template
+            const pesanLinkUndangan = text
+                .replace(/{nama_tamu}/g, nama)
+                .replace(/{nama_group}/g, group || '') // Fallback jika group null
+                .replace(/{kode_tamu}/g, kodetamu || '') // Fallback jika kode tamu null
+                .replace(/{link_undangan}/g, linkUndangan);
 
-    // 5. Encode seluruh pesan agar aman untuk URL WhatsApp
-    //    Ini lebih aman daripada hanya mengganti \n
-    const convertedValue = encodeURIComponent(pesanLinkUndangan);
+            // 5. Encode seluruh pesan agar aman untuk URL WhatsApp
+            //    Ini lebih aman daripada hanya mengganti \n
+            const convertedValue = encodeURIComponent(pesanLinkUndangan);
 
-    // 6. Buka link WhatsApp di tab baru
-    if (whatsapp) {
-        // Jika nomor WhatsApp ada, kirim langsung ke nomor tersebut
-        window.open(`https://api.whatsapp.com/send/?phone=62${whatsapp}&text=${convertedValue}`, '_blank');
-    } else {
-        // Jika tidak ada nomor, buka WhatsApp untuk dibagikan secara manual
-        window.open(`https://api.whatsapp.com/send/?text=${convertedValue}`, '_blank');
-    }
-}
+            // 6. Buka link WhatsApp di tab baru
+            if (whatsapp) {
+                // Jika nomor WhatsApp ada, kirim langsung ke nomor tersebut
+                window.open(`https://api.whatsapp.com/send/?phone=62${whatsapp}&text=${convertedValue}`, '_blank');
+            } else {
+                // Jika tidak ada nomor, buka WhatsApp untuk dibagikan secara manual
+                window.open(`https://api.whatsapp.com/send/?text=${convertedValue}`, '_blank');
+            }
+        }
 
         const editTamu = (id) => {
             $.ajax({
