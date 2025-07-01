5daeaf8ffe928bf609216fb5031d030c1b8f37cd
rickyfauzi <rickyfauzi116@gmail.com>
2025-07-01 18:53:06 +0700

    dda

--- resources/views/tema-undangan/tema3.blade.php
+++ resources/views/tema-undangan/tema3.blade.php
@@ -418,7 +418,7 @@
             color: var(--accent-color-1);
             position: relative;
             z-index: 1;
-            background-image: url("{{ asset('tema3/img/bg.png') }}");
+            background-image: url("{{ asset('tema3/img/bg2.png') }}");
             background-size: cover;
             padding-top: 80px;
             padding-bottom: 40px;
